42ad6453c41c70c1fddb411a0a7e9a26e7e52307
Полина Куранова <polinakuranova@MacBook-Pro-Polina.local>
2023-05-16 11:16:56 +0300

    first js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function start() {
+    document.getElementById("parametrs_form_h").hidden = true;
+    document.getElementById("solution_text_h").hidden = true;
+    document.getElementById("chart_div_h").hidden = true;
+    document.getElementById("theory_text_h").hidden = true;
+}
+
+function roll(id, button_id) {
+    let elem = document.getElementById(id);
+    let elem_h = document.getElementById(id + '_h');
+    let button = document.getElementById(button_id);
+
+    if (elem.hidden == false) {
+        elem.hidden = true;
+        elem_h.hidden = false;
+        button.textContent = '+';
+    } else {
+        elem.hidden = false;
+        elem_h.hidden = true;
+        button.textContent = '-';
+    }
+}
