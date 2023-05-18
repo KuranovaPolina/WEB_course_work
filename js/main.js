386ab0ed095cd995a20e9639424d25f91aa653ad
Полина Куранова <polinakuranova@MacBook-Pro-Polina.local>
2023-05-18 11:56:09 +0300

    added jquery.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 function start() {
-    document.getElementById("parametrs_form_h").hidden = true;
-    document.getElementById("solution_text_h").hidden = true;
-    document.getElementById("chart_div_h").hidden = true;
-    document.getElementById("theory_text_h").hidden = true;
+    $("#parametrs_form_h").hide();
+    $("#solution_text_h").hide();
+    $("#chart_div_h").hide();
+    $("#theory_text_h").hide();
 }
 
 function roll(id, button_id) {
-    let elem = document.getElementById(id);
-    let elem_h = document.getElementById(id + '_h');
     let button = document.getElementById(button_id);
 
-    if (elem.hidden == false) {
-        elem.hidden = true;
-        elem_h.hidden = false;
+    $("#" + id + "_h").slideToggle();
+    $("#" + id).slideToggle();
+    
+    if (button.textContent == '-') {
         button.textContent = '+';
     } else {
-        elem.hidden = false;
-        elem_h.hidden = true;
         button.textContent = '-';
     }
+
 }
 
 function solve() {
